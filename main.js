009163aa2a7e63203d7d854741e040beb55d13dc
Nicholas Hoyt <nhoyt@illinois.edu>
2022-09-01 17:18:45 -0500

    Update main.js

--- main.js
+++ main.js
@@ -13,12 +13,25 @@ class DisclosureMenu {
       console.log(button.getAttribute('aria-controls'));
 
       button.addEventListener('click', evt => {
+        this.toggleSubmenu(evt.target);
       });
     }
   }
 
   getSubmenu (button) {
-    const id = button.getAttribute('aria-controls');
-    return document.getElementById(id);
+    const idSubmenu = button.getAttribute('aria-controls');
+    return document.getElementById(idSubmenu);
+  }
+
+  toggleSubmenu (button) {
+    const submenu = this.getSubmenu(button);
+    if (button.getAttribute('aria-expanded') === 'true') {
+      button.setAttribute('aria-expanded', 'false');
+      submenu.style.display = 'none';
+    }
+    else {
+      button.setAttribute('aria-expanded', 'true');
+      submenu.style.display = 'block';
+    }
   }
 }
